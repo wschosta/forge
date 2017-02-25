047e987a4703f0d4428a6fde3555809a5c135a9a
wschosta <wschosta@gmail.com>
2017-02-25 18:29:54 -0500

    Final changes before in-depth run
    Just some tweaks. Tester will first execute the la.main script and then do the monte carlo analysis for each of the three states under investigation

--- tester.m
+++ tester.m
@@ -4,13 +4,30 @@
 
 fclose all; close all; clc; clear all;
 
+try
+    fprintf('**************************************LA MASTER ANALYSIS**************************************\n')
+    
+    la_time = tic;
+    
+    la.main()
+    
+    fprintf('**************************************LA COMPLETE!**************************************\n')
+    toc(la_stime)
+catch e
+    warning('ERROR: %s',e.message)
+    fprintf('**************************************LA FAILED!**************************************\n')
+    toc(la_time)
+    la_errors = e;
+end
+
+
 master = tic;
 
 states = {'WI' 'IN' 'OR'};
 
 errors = {};
 
-monte_carlo_number_list = [10];% 100 1000 10000 50000 100000];
+monte_carlo_number_list = [10 100 1000 5000 10000 50000 100000];
 
 for j = 1:length(monte_carlo_number_list)
     for i = 1:length(states)
@@ -24,22 +41,22 @@ for j = 1:length(monte_carlo_number_list)
         a.recompute = true;
         a.reprocess = true;
         a.generate_outputs     = true;
-        a.predict_montecarlo   = false;
-        a.recompute_montecarlo = false;
+        a.predict_montecarlo   = true;
+        a.recompute_montecarlo = true;
         
         a.monte_carlo_number = monte_carlo_number_list(j);
         
-%         try
+        try
             a.run();
             
             fprintf('**************************************%s COMPLETE!**************************************\n',states{i})
             toc(state_time)
-%         catch e
-%             warning('ERROR: %s',e.message)
-%             fprintf('**************************************%s FAILED!**************************************\n',states{i})
-%             toc(state_time)
-%             errors{end+1} = e; %#ok<SAGROW>
-%         end
+        catch e
+            warning('ERROR: %s',e.message)
+            fprintf('**************************************%s FAILED!**************************************\n',states{i})
+            toc(state_time)
+            errors{end+1} = e; %#ok<SAGROW>
+        end
     end
 end
 close all; fclose all;
