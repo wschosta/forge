9ad91d1fc7e9d7c8d6f5549dd47e61871b486da6
wschosta <wschosta@users.noreply.github.com>
2017-10-20 11:44:16 -0400

    Merge Data Issue
    Fixed some issues in the  data merge, wasn't picking up the MC runs just the non-MC runs (what did I do all those runs for if not to use them??). Also fixed a minor bug where it wasn't taking the correct difference between the fixed K and variable K scores

--- +finance/mergeData.m
+++ +finance/mergeData.m
@@ -9,45 +9,72 @@ merged_data.ID = [];
 
 % name_parts = regexp(merged_data.name,'[, ]','split');
 
-files_to_match = dir(sprintf('data/%s/elo_model/*.csv',state));
+file_locations = {sprintf('data/%s/elo_model/*.csv',state), sprintf('data/%s/elo_model/MC/*.csv',state)};
 
-for i = 1:length(files_to_match)
-    read_file = readtable([files_to_match(i).folder '/' files_to_match(i).name]); 
+fprintf('START----- Finance Data Merge for %s:\n',state)
+fprintf('File Locations:\n')
+fprintf('\t\t%s\n',file_locations{:})
+
+fprintf('\n\nBEGIN:\n')
+
+start_time = tic;
+
+delete_str = '';
+
+for f = 1:length(file_locations)
     
-    full_name = cell(height(read_file),1);
+    files_to_match = dir(file_locations{f});
     
-    for j = 1:height(read_file)
-        merged_name = read_file.last_name{j};
+    for i = 1:length(files_to_match)
         
-        if ~isempty(read_file.suffix{j})
-            merged_name = sprintf('%s %s, %s',merged_name,read_file.suffix{j},read_file.first_name{j});
-        else
-            merged_name = sprintf('%s, %s', merged_name,read_file.first_name{j});
+        
+        read_file = readtable([files_to_match(i).folder '/' files_to_match(i).name]);
+        
+        full_name = cell(height(read_file),1);
+        
+        for j = 1:height(read_file)
+            
+            print_str = sprintf('%i %i %i',f,i,j);
+            fprintf([delete_str,print_str]);
+            delete_str = repmat(sprintf('\b'),1,length(print_str));
+            
+            merged_name = read_file.last_name{j};
+            
+            if ~isempty(read_file.suffix{j})
+                merged_name = sprintf('%s %s, %s',merged_name,read_file.suffix{j},read_file.first_name{j});
+            else
+                merged_name = sprintf('%s, %s', merged_name,read_file.first_name{j});
+            end
+            
+            if ~isempty(read_file.middle_name{j})
+                merged_name = sprintf('%s %s',merged_name,read_file.middle_name{j});
+            end
+            
+            if ~isempty(read_file.nickname{j})
+                merged_name = sprintf('%s (%s)',merged_name,read_file.nickname{j});
+            end
+            
+            merged_name = upper(regexprep(merged_name,'\.',''));
+            
+            full_name{j} = merged_name;
+            
         end
         
-        if ~isempty(read_file.middle_name{j})
-            merged_name = sprintf('%s %s',merged_name,read_file.middle_name{j});
-        end
+        read_file.full_name = full_name;
         
-        if ~isempty(read_file.nickname{j})
-            merged_name = sprintf('%s (%s)',merged_name,read_file.nickname{j});
-        end
+        a = util.CStrAinBP(merged_data.full_name,full_name);
+        b = util.CStrAinBP(full_name,merged_data.full_name);
         
-        merged_name = upper(regexprep(merged_name,'\.',''));
+        total_merge = join(read_file(b,:),merged_data(a,:));
         
-        full_name{j} = merged_name;
-        
+        writetable(total_merge,sprintf('data/%s/merged_data/%s.csv',state,files_to_match(i).name))
     end
     
-    read_file.full_name = full_name;
-    
-    a = util.CStrAinBP(merged_data.full_name,full_name);
-    b = util.CStrAinBP(full_name,merged_data.full_name);
-    
-    total_merge = join(read_file(b,:),merged_data(a,:));
-    
-    writetable(total_merge,sprintf('data/%s/merged_data/%s.csv',state,files_to_match(i).name))
 end
 
+print_str = sprintf('Finished!');
+fprintf([delete_str,print_str]);
+
+toc(start_time)
 
 end
